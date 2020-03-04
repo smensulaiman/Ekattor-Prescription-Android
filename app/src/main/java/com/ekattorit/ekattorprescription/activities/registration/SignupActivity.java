0e9594cbf3ef00710e66d314f96b08500166e463
smensulaiman <smensulaiman007@gmail.com>
2020-03-04 23:33:53 +0600

    init

---
+++ app/src/main/java/com/ekattorit/ekattorprescription/activities/registration/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.ekattorit.ekattorprescription.activities.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ekattorit.ekattorprescription.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
