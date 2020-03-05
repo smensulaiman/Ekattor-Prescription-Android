a02d05896e0510fd54d6f6f22ade7ef10e2375b6
smensulaiman <smensulaiman007@gmail.com>
2020-03-05 22:04:46 +0600

    Splash Screen Added

--- app/src/main/java/com/ekattorit/ekattorprescription/activities/welcome/SplashActivity.java
+++ app/src/main/java/com/ekattorit/ekattorprescription/activities/welcome/SplashActivity.java
@@ -2,7 +2,12 @@ package com.ekattorit.ekattorprescription.activities.welcome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.ekattorit.ekattorprescription.R;
 
@@ -12,5 +17,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        changeStatusBarColor();
+
     }
+
+    private void changeStatusBarColor() {
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
 }
